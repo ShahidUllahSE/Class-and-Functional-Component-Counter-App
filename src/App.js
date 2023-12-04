0c60337a139d4b70cb142acedd7cf8fc90d340bd
ShahidUllahSE <shahidbangash8899@gmail.com>
2023-12-04 15:00:57 +0500

    Class and functional Component counter app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import ClassComponentCounterApp from './components/ClassComponentCounterApp';
+import FunctionalCounterApp from './components/FunctionalCounterApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        <ClassComponentCounterApp/>
+        <FunctionalCounterApp/>
+
+
     </div>
   );
 }
